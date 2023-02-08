a59c899f74d9b343c68c16d6004d8d3d983097fc
Brama Udi <bramaudi.al@gmail.com>
2023-02-08 15:58:26 +0700

    Convert RSS to JSON

--- api/index.php
+++ api/index.php
@@ -5,6 +5,7 @@ $source_code = file($_GET['url']);
 $xml = '';
 
 header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
 // 1. traversing through each element of the array
 // 2. printing their subsequent HTML entities
@@ -13,4 +14,7 @@ foreach ($source_code as $line_number => $last_line) {
     $xml .= $last_line;
 }
 
-echo $xml;
+$xml = simplexml_load_string($xml);
+$json = json_encode($xml);
+
+echo $json;
