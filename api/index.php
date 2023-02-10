ed4fd137501eea8295af5f147151b4aa8fee25ca
Brama Udi <bramaudi.al@gmail.com>
2023-02-10 20:19:23 +0700

    Add library

--- api/index.php
+++ api/index.php
@@ -1,26 +1,13 @@
 <?php
 
+require_once '../Feed.php';
+
 $url = $_GET['url'];
 
 if (empty($url)) {
     die('Parameter `url` cannot be empty');
 }
 
-// Storing the elements of the webpage into an array
-$source_code = file($url);
-$xml = '';
+$rss = Feed::load($url);
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-// 1. traversing through each element of the array
-// 2. printing their subsequent HTML entities
-foreach ($source_code as $line_number => $last_line) {
-    // echo nl2br(htmlspecialchars($last_line) . "\n");
-    $xml .= $last_line;
-}
-
-$xml = simplexml_load_string($xml);
-$json = json_encode($xml);
-
-echo $json;
+echo $rss;
