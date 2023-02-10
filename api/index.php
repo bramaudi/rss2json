d8ed52d0ea0aeb30dc703afd74f4e441652f012f
Brama Udi <bramaudi.al@gmail.com>
2023-02-10 23:00:24 +0700

    Fix error

--- api/index.php
+++ api/index.php
@@ -1,7 +1,9 @@
 <?php
 
-use FeedIo\Adapter\Http\Client;
-use FeedIo\FeedIo;
+use FeedIo\Feed;
+use FeedIo\Parser\XmlParser;
+use FeedIo\Reader\Document;
+use FeedIo\Standard\Rss;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -12,10 +14,18 @@ if (empty($url)) {
     $error = 1;
 }
 
-$client = new Client(new Symfony\Component\HttpClient\HttplugClient());
-$feedIo = new FeedIo($client);
+// new DateTimeBuilder : it will be used by the parser to convert formatted dates into DateTime instances
+$dateTimeBuilder = new \FeedIo\Rule\DateTimeBuilder();
 
-$result = $feedIo->read($url);
+// new Standard\\Rss : it will provide all standard specific rules to the parser
+$standard = new Rss($dateTimeBuilder);
+
+$feedIo = new XmlParser($standard, new \Psr\Log\NullLogger());
+
+// we load it using the Dom library
+$document = new Document(file_get_contents($url));
+
+$result = $feedIo->parse($document, new Feed);
 
 header('Content-type: application/json');
-echo json_encode($result->getFeed());
+echo json_encode($result);
