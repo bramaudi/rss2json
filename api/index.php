1514130181dd13bf060a37e6e675b24b0a0b8f93
Brama Udi <bramaudi.al@gmail.com>
2023-02-24 19:57:50 +0700

    Add feed url discovery

--- api/index.php
+++ api/index.php
@@ -3,6 +3,7 @@ error_reporting(E_ERROR | E_PARSE);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use FeedIo\Feed;
+use FeedIo\FeedIo;
 use FeedIo\Parser\XmlParser;
 use FeedIo\Reader\Document;
 use FeedIo\Standard\Atom;
@@ -21,7 +22,18 @@ try {
     // new DateTimeBuilder : it will be used by the parser to convert formatted dates into DateTime instances
     $dateTimeBuilder = new \FeedIo\Rule\DateTimeBuilder();
 
-    $xml = new SimpleXMLElement($html, LIBXML_NOWARNING | LIBXML_NOERROR | LIBXML_NOCDATA);
+    try {
+        $xml = new SimpleXMLElement($html, LIBXML_NOWARNING | LIBXML_NOERROR | LIBXML_NOCDATA);
+    } catch (\Throwable $th) {
+        $client = new \FeedIo\Adapter\Http\Client(new Symfony\Component\HttpClient\HttplugClient());
+        $feedIo = new FeedIo($client);
+        
+        $feeds = $feedIo->discover($url);
+        $origin = parse_url($url, PHP_URL_SCHEME) . '://' . parse_url($url, PHP_URL_HOST);
+        $feedUrl = count($feeds) ?  $origin.$feeds[0] : '';
+        $message = $feedUrl ? "Found valid feed url in $feedUrl" : $th->getMessage();
+        throw new Exception($message);
+    }
 
     if ($xml->channel) {
         $standard = new Rss($dateTimeBuilder);
@@ -50,8 +62,7 @@ try {
             'link' => $rss->getLink(),
             'description' => $rss->getDescription(),
         ],
-        'items' => array_map(function ($item)
-        {
+        'items' => array_map(function ($item) {
             unset($item['categories']);
             unset($item['publicId']);
             unset($item['host']);
